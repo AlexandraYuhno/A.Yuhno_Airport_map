0fe2e26e9822d9da5c0203b1da439b9de1fed3df
Yuhno Alexandra <a.yuhno@tg.dunice.net>
2024-09-27 12:25:00 +0300

    delete spaces

--- src/components/AirportMap.vue
+++ src/components/AirportMap.vue
@@ -10,69 +10,70 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, watch } from 'vue';
-import { useAirportStore } from '../store/airportModule'; 
-import MapService from '../mapServises'; 
-import { Airport } from '../store/types';
+  import { ref, computed, onMounted, watch } from 'vue';
 
-const airportStore = useAirportStore();
+  import { useAirportStore } from '../store/airportModule'; 
+  import MapService from '../mapServises'; 
+  import { Airport } from '../store/types';
 
-const airports = computed(() => airportStore.airports);
-const loading = computed(() => airportStore.isLoading);
-const error = computed(() => airportStore.error);
+  const airportStore = useAirportStore();
 
-const mapContainer = ref<HTMLElement | null>(null);
-let mapService: MapService | null = null;
+  const airports = computed(() => airportStore.airports);
+  const loading = computed(() => airportStore.isLoading);
+  const error = computed(() => airportStore.error);
 
-const initMap = () => {
-  if (mapContainer.value) {  
-    mapService = new MapService(mapContainer.value);
-    mapService.initMap();
-  }
-};
+  const mapContainer = ref<HTMLElement | null>(null);
+  let mapService: MapService | null = null;
 
-const addMarkersToMap = (airportData: Airport[]) => {
-  if (mapService && mapService.isMapInitialized()) {
-    mapService.addMarkers(airportData);
-  }
-};
+  const initMap = () => {
+    if (mapContainer.value) {  
+      mapService = new MapService(mapContainer.value);
+      mapService.initMap();
+    }
+  };
 
-watch(airports, (newAirports) => {
-  if (newAirports.length > 0) {
-    addMarkersToMap(newAirports);
-  }
-});
+  const addMarkersToMap = (airportData: Airport[]) => {
+    if (mapService && mapService.isMapInitialized()) {
+      mapService.addMarkers(airportData);
+    }
+  };
 
-onMounted(() => {
-  const icaoCodes = ['EGLL', 'LFPG', 'LTBA', 'EDDF', 'EHAM'];
-  airportStore.fetchAirportData(icaoCodes);
-  initMap(); 
-});
+  watch(airports, (newAirports) => {
+    if (newAirports.length > 0) {
+      addMarkersToMap(newAirports);
+    }
+  });
+
+  onMounted(() => {
+    const icaoCodes = ['EGLL', 'LFPG', 'LTBA', 'EDDF', 'EHAM'];
+    airportStore.fetchAirportData(icaoCodes);
+    initMap(); 
+  });
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #555;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #555;
+  }
 
-.container {
-  text-align: center;
-  min-width: 800px;
-  padding: 20px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  background-color: #000;
-  border-radius: 8px;
-  color: #fff;
-}
+  .container {
+    text-align: center;
+    min-width: 800px;
+    padding: 20px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    background-color: #000;
+    border-radius: 8px;
+    color: #fff;
+  }
 
-#map {
-  min-width: 100%;
-  height: 600px;
-}
+  #map {
+    min-width: 100%;
+    height: 600px;
+  }
 </style>

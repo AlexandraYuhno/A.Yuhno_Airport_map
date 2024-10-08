ed4bf255d363716a92997e16a5a1a092632ac682
Yuhno Alexandra <a.yuhno@tg.dunice.net>
2024-10-08 11:17:26 +0300

    removed extra spaces

--- src/api/apiClientWeather.ts
+++ src/api/apiClientWeather.ts
@@ -3,22 +3,22 @@ import axios, { AxiosInstance } from 'axios';
 const apiKeyWeather = import.meta.env.VITE_API_KEY_WEATHER;
 
 const apiClientWeather: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_WEATHER_URL,
-    headers: {
-      "X-Api-Key": apiKeyWeather,
-    },
-  });
+  baseURL: import.meta.env.VITE_API_WEATHER_URL,
+  headers: {
+    "X-Api-Key": apiKeyWeather,
+  },
+});
 
 export const fetchWeather = (latitude: number, longitude: number) => {
-    return apiClientWeather
-      .get(`=${latitude},${longitude}&units=metric&apikey=${apiKeyWeather}`)
-      .then((response) => {
-        const res = response.data;
-        return res;
-      })
-      .catch((error) => {
-        console.error("Error fetching weather data:", error);
-      });
+  return apiClientWeather
+    .get(`=${latitude},${longitude}&units=metric&apikey=${apiKeyWeather}`)
+    .then((response) => {
+      const res = response.data;
+      return res;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
   };
   
   export default apiClientWeather;
